240acc4669ce15f1ada423a6a91afad0c7244608
Shivam Singhal <shivamsinghal0610@gmail.com>
2017-07-03 23:25:16 +0530

    updated

---
+++ LOC JUNE17/HHMGN.cpp
@@ -0,0 +1,33 @@
+#include <iostream>
+#include <bits/stdc++.h>
+using namespace std;
+//find a number K from array A such that if all elements in A greater than K are changed to K, the sum of all elements in the resulting array will be S. 
+int main()
+{
+    int t,n,i,f=0;
+    long long s,sum1=0,sum2=0;
+    cin >> t;
+    while(t--)
+    {
+        cin >> n >> s;
+        int a[n];
+        for(i=0;i<n;i++)
+        cin >>a[i];
+        sort(a,a+n);
+        for(i=0;i<n;i++)
+        {
+            sum1+=a[i];
+            sum2=(a[i]*(n-i-1));
+            if((sum1+sum2)==s)
+            {
+                cout << a[i] <<endl;
+                f=1;
+                break;
+            }
+        }
+        if(f==0)
+         cout << -1 << endl;
+         sum1=0;sum2=0;f=0;
+    }
+	return 0;
+}
